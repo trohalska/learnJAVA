1576d0ba5ed5a6e5ad62ea9e1702fb56db9f0cbf
Tetiana Rohalska <trogalska2208@gmail.com>
2021-01-06 14:05:04 +0200

    delete not java core

--- SQL/create.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE `sales`;
-CREATE TABLE `customer` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(45) NOT NULL,
-    `email` varchar(45) NOT NULL,
-    `address` varchar(45) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
